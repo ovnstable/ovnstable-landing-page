dbd1c6f78873f151dd0459eb09001524bb7738e2
Roman <romangolovlyov@gmail.com>
2023-11-09 14:35:41 +0500

    Step cards component added

--- src/components/content/AboutOvernightComponent.vue
+++ src/components/content/AboutOvernightComponent.vue
@@ -1,10 +1,72 @@
 <template>
    <div>
-       <div>
+       <div class="carousel">
             <PartnersCarousel />
        </div>
-       <div>
-           <StepCards></StepCards>
+       <div class="cards-container" v-if="isDesktop">
+           <div class="single-card">
+               <StepCards
+                   step-label="Step 1"
+                   image-link="mint-tokens.svg"
+                   legend-title="MINT TOKENS+"
+                   sub-title="Instantly mint any amount of Tokens+"
+               />
+           </div>
+           <div class="arrow-container">
+               <img :src="require('@/assets/cards/arrow-right.svg')" alt="">
+           </div>
+           <div class="single-card">
+               <StepCards
+                   step-label="Step 2"
+                   image-link="receive-yield.svg"
+                   legend-title="RECEIVE YIELD"
+                   sub-title="Do nothing and receive daily rebase by holding Tokens+"
+               />
+           </div>
+           <div class="arrow-container">
+               <img :src="require('@/assets/cards/arrow-right.svg')" alt="">
+           </div>
+           <div class="single-card">
+               <StepCards
+                   step-label="Step 3"
+                   image-link="use-in-defi.svg"
+                   legend-title="USE IN DeFI"
+                   sub-title="Use your Tokens+ in DeFI to maximize your yield"
+               />
+           </div>
+       </div>
+
+       <div class="cards-container-mobile" v-if="isMobile">
+           <div class="single-card">
+               <StepCardsMobile
+                   step-label="Step 1"
+                   image-link="mint-tokens.svg"
+                   legend-title="MINT TOKENS+"
+                   sub-title="Instantly mint any amount of Tokens+"
+               />
+               <div class="arrow-container-mobile">
+                   <img class="arrow-mobile" :src="require('@/assets/cards/arrow-bottom.svg')" alt="Arrow bottom">
+               </div>
+           </div>
+           <div class="single-card">
+               <StepCardsMobile
+                   step-label="Step 2"
+                   image-link="receive-yield.svg"
+                   legend-title="RECEIVE YIELD"
+                   sub-title="Do nothing and receive daily rebase by holding Tokens+"
+               />
+               <div class="arrow-container-mobile">
+                   <img class="arrow-mobile" :src="require('@/assets/cards/arrow-bottom.svg')" alt="Arrow bottom">
+               </div>
+           </div>
+           <div class="single-card">
+               <StepCardsMobile
+                   step-label="Step 3"
+                   image-link="use-in-defi.svg"
+                   legend-title="USE IN DeFI"
+                   sub-title="Use your Tokens+ in DeFI to maximize your yield"
+               />
+           </div>
        </div>
    </div>
 </template>
@@ -12,17 +74,88 @@
 <script>
 
 import StepCards from "@/components/tools/StepCards";
+import StepCardsMobile from "@/components/tools/StepCardsMobile";
 import PartnersCarousel from "@/components/tools/PartnersCarousel";
+import {mapState} from "vuex";
+
 export default {
     name: "AboutOvernightComponent",
     components: {
         PartnersCarousel,
-        StepCards
-    }
+        StepCards,
+        StepCardsMobile
+    },
+
+    computed: {
+        ...mapState('device', ['deviceType', "deviceOrientation", 'isMobile', "isTablet", 'isDesktop']),
+    },
 }
 </script>
 
 
 <style scoped>
+/* mobile */
+@media only screen and (max-width: 1024px) {
+    .single-card {
+        position: relative;
+        max-width: 500px;
+    }
+    .single-card:first-of-type {
+        padding-top: 20px;
+    }
 
+    .arrow-container-mobile {
+        position: relative;
+        top: 0;
+        right: -290px;
+    }
+}
+
+/* desktop */
+@media only screen and (min-width: 1400px) {
+    .cards-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        margin-left: 300px;
+        margin-right: 300px;
+        margin-bottom: 100px;
+    }
+
+    .carousel {
+        margin-top: 10px;
+        margin-bottom: 40px;
+    }
+
+
+    .arrow-container {
+        margin-bottom: 30px;
+    }
+}
+
+@media
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 1024px),
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 1024px),
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 1024px),
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 1024px),
+only screen and (                min-resolution: 192dpi) and (min-width: 1024px),
+only screen and (                min-resolution: 2dppx)  and (min-width: 1024px) {
+    .cards-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        margin-left: 100px;
+        margin-right: 100px;
+        margin-bottom: 100px;
+    }
+
+    .carousel {
+        margin-top: 10px;
+        margin-bottom: 40px;
+    }
+
+    .arrow-container {
+        margin-bottom: 30px;
+    }
+}
 </style>
--- src/components/tools/StepCards.vue
+++ src/components/tools/StepCards.vue
@@ -1,22 +1,124 @@
 <template>
-    <div class="sub-title">
-
+    <div class="main-sub-title">
+        {{ stepLabel }}
+    </div>
+    <div class="cards-container">
+        <div class="step-card-box">
+            <img :src="require('@/assets/cards/' + imageLink)" alt="About products image">
+        </div>
+        <div class="sub-title">
+            {{legendTitle}}
+        </div>
+        <div class="paragraph">
+            {{subTitle}}
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "StepCards",
-    components: {
 
-    },
+    props: {
+        stepLabel: {
+            type: String,
+            default: '',
+        },
 
-    data() {
+        imageLink: {
+            type: String,
+            default: null,
+        },
 
+        legendTitle: {
+            type: String,
+            default: '',
+        },
+
+        subTitle: {
+            type: String,
+            default: '',
+        },
     },
 }
 </script>
 
 <style scoped>
+/* mobile */
+@media only screen and (max-width: 1024px) {
 
+}
+
+/* desktop */
+@media only screen and (min-width: 1400px) {
+    .cards-container {
+        display: flex;
+        flex-direction: column;
+        max-width: 260px;
+        text-align: center;
+    }
+
+    .step-card-box {
+        width: 253px;
+        height: 253px;
+        border: 1px solid #0F172A;
+        border-bottom: 2px solid #0F172A;
+        background-color: white;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .main-sub-title {
+        text-transform: none;
+        margin-bottom: 10px;
+        margin-top: 20px;
+    }
+
+    .sub-title {
+        margin-top: 10px;
+        margin-bottom: 20px;
+        text-transform: none;
+    }
+}
+
+@media
+only screen and (-webkit-min-device-pixel-ratio: 2)      and (min-width: 1024px),
+only screen and (   min--moz-device-pixel-ratio: 2)      and (min-width: 1024px),
+only screen and (     -o-min-device-pixel-ratio: 2/1)    and (min-width: 1024px),
+only screen and (        min-device-pixel-ratio: 2)      and (min-width: 1024px),
+only screen and (                min-resolution: 192dpi) and (min-width: 1024px),
+only screen and (                min-resolution: 2dppx)  and (min-width: 1024px) {
+    .cards-container {
+        display: flex;
+        flex-direction: column;
+        max-width: 260px;
+        text-align: center;
+    }
+
+    .step-card-box {
+        width: 253px;
+        height: 253px;
+        border: 1px solid #0F172A;
+        border-bottom: 2px solid #0F172A;
+        background-color: white;
+
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .main-sub-title {
+        text-transform: none;
+        margin-bottom: 10px;
+        margin-top: 20px;
+    }
+
+    .sub-title {
+        margin-top: 10px;
+        margin-bottom: 20px;
+    }
+}
 </style>

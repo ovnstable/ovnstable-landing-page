b8eb6f43c9a69545f027910ed5d0fffc7ce9f3a0
y.pavlov <lenvonder12vorh@gmail.com>
2023-11-20 15:43:42 +0300

    Add reset cache in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ token=$1
 url=$2
 stand=$3
 dockerComposePath="/home/ubuntu/docker-compose.yaml"
-
+zoneId=$4
+apiKey=$5
 
 if [ "$stand" = "prod" ]
 then
@@ -43,3 +44,15 @@ ssh $url docker login \
 
 ssh $url docker pull cr.yandex/crpg11k469bhc8lch9gm/overnight/landing-page:$tag
 ssh $url docker-compose -f $dockerComposePath up -d --no-deps $nameDapp
+
+
+# Reset cache on Cloudflare
+# Affects all domains
+
+curl --request POST \
+  --url https://api.cloudflare.com/client/v4/zones/$zoneId/purge_cache \
+  --header 'Content-Type: application/json' \
+  --header "Authorization: Bearer $apiKey" \
+  --data '{
+  "purge_everything": true
+}'
